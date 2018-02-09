6309bdd75cc18cb9abdeb5a3e65c58fba2b7f7a9
Will Pinkston <william@pinkston.com>
2018-02-09 10:10:55 -0700

    Added a package to all client and common files to reflect required Android structure.

---
+++ src/cs_340/client/model/observable/IStateChange.java
@@ -0,0 +1,9 @@
+package cs_340.client.model.observable;
+
+public interface IStateChange {
+	
+  public int getCurrentStateCode();
+  
+  public Object getStateData();
+
+}
---
+++ src/cs_340/common/ICommand.java
@@ -0,0 +1,9 @@
+package cs_340.common;
+
+/**
+ * Created by Pultilian on 1/22/2018.
+ */
+
+public interface ICommand {
+    public Object execute();
+}
